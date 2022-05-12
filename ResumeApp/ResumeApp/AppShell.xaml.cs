d41b50796f47e18c9caba982c1f688017b5bb748
Zach <z.wilkin13@gmail.com>
2022-05-12 19:09:48 -0400

    added solution file to repository

---
+++ ResumeApp/ResumeApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ResumeApp.ViewModels;
+using ResumeApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ResumeApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ ResumeApp/ResumeApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ResumeApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ResumeApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
